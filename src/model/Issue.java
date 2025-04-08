ac60978d84b49af4270b152ca8442130cf5cc290
Hemalatha E <hema68208102@gmail.com>
2025-04-08 11:59:33 +0530

    Update Issue.java

--- src/model/Issue.java
+++ src/model/Issue.java
@@ -1,21 +1,21 @@
 package model;
 
 public class Issue {
-	private String regno;
-	private String name;
-	private String code;
-	private String issueDate;
+    private String regno;
+    private String name;
+    private String code;
+    private String issueDate;
 
-	// Getters & Setters
-	public String getRegno() { return regno; }
-	public void setRegno(String regno) { this.regno = regno; }
+    // Getters & Setters
+    public String getRegno() { return regno; }
+    public void setRegno(String regno) { this.regno = regno; }
 
-	public String getName() { return name; }
-	public void setName(String name) { this.name = name; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
 
-	public String getCode() { return code; }
-	public void setCode(String code) { this.code = code; }
+    public String getCode() { return code; }
+    public void setCode(String code) { this.code = code; }
 
-	public String getIssueDate() { return issueDate; }
-	public void setIssueDate(String issueDate) { this.issueDate = issueDate; }
+    public String getIssueDate() { return issueDate; }
+    public void setIssueDate(String issueDate) { this.issueDate = issueDate; }
 }
