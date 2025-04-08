d8b30b6b189acd58476899758cb60eefbb11e7dc
Hemalatha E <hema68208102@gmail.com>
2025-04-08 12:02:57 +0530

    library_db.sql

--- resource/library_db.sql
+++ resource/library_db.sql
@@ -1,18 +1,19 @@
 CREATE DATABASE library_db;
+
 USE library_db;
 
 CREATE TABLE books (
-  name VARCHAR(50) NOT NULL,
-  author VARCHAR(50) NOT NULL,
-  code VARCHAR(50) PRIMARY KEY,
-  total INT NOT NULL,
-  subject VARCHAR(50) NOT NULL
+    name VARCHAR(255),
+    author VARCHAR(255),
+    code VARCHAR(50) PRIMARY KEY,
+    total INT,
+    subject VARCHAR(100)
 );
 
 CREATE TABLE issue (
-  regno VARCHAR(50) PRIMARY KEY,
-  name VARCHAR(50) NOT NULL,
-  code VARCHAR(50) NOT NULL,
-  issue_date VARCHAR(50) NOT NULL,
-  FOREIGN KEY (code) REFERENCES books(code)
+    regno VARCHAR(50),
+    name VARCHAR(255),
+    code VARCHAR(50),
+    issueDate DATE,
+    FOREIGN KEY (code) REFERENCES books(code)
 );
